e0791f5e73bc12ff87cc0863cf889a6d3d5afccb
Domenico Luciani <domenicoleoneluciani@gmail.com>
2024-01-05 22:17:31 +0100

    Simplified flag and remove parameters and parser

--- main.go
+++ main.go
@@ -1,70 +1,89 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"gowc/parameters"
 	"gowc/reader"
 	readerBytes "gowc/reader/bytes"
 	readerChars "gowc/reader/chars"
 	readerLines "gowc/reader/lines"
 	readerWords "gowc/reader/words"
+	"gowc/utils"
+	"io/fs"
 	"os"
 )
 
+type FSReader struct {
+	reader   reader.WcReaderManager
+	filename string
+}
+
 func main() {
-	parametersMap := parameters.GetParameters()
+	paramsDefMap := setParameters()
+	paramsMap := getParametersFromCLI(paramsDefMap)
+	readers := getFsReadersByParameters(paramsMap)
+	countWithReadersAndPrint(readers)
+}
 
-	if parametersMap["c"] != "" {
-		numBytes, filename := readNumberOfBytesFrom(parametersMap["c"])
-		printNumbers(numBytes, filename)
-	}
-
-	if parametersMap["l"] != "" {
-		numLines, fileName := readNumberOfLinesFrom(parametersMap["l"])
-		printNumbers(numLines, fileName)
-	}
-
-	if parametersMap["w"] != "" {
-		numWords, fileName := readNumberOfWordsFrom(parametersMap["w"])
-		printNumbers(numWords, fileName)
-	}
-
-	if parametersMap["m"] != "" {
-		numChars, fileName := readNumberOfCharsFrom(parametersMap["m"])
-		printNumbers(numChars, fileName)
+func countWithReadersAndPrint(readers []FSReader) {
+	for _, r := range readers {
+		n, filename := count(r.reader, r.filename)
+		printNumbers(n, filename)
 	}
 }
 
-func readNumberOfLinesFrom(linesCountFilepath string) (int64, string) {
-	filesystemDir, fileName := parameters.GetFSandFilenameFromParameter(linesCountFilepath)
-	wcLinesReader := readerLines.NewWcLinesReader(filesystemDir)
-
-	return count(wcLinesReader, fileName)
+func getParametersFromCLI(paramsDefMap map[string]string) map[string]string {
+	paramsMap := map[string]string{}
+	for p, _ := range paramsDefMap {
+		paramsMap[p] = flag.Lookup(p).Value.String()
+	}
+	return paramsMap
 }
 
-func readNumberOfBytesFrom(bytesCountFilepath string) (int64, string) {
-	filesystemDir, fileName := parameters.GetFSandFilenameFromParameter(bytesCountFilepath)
-	wcBytesReader := readerBytes.NewWcBytesReader(filesystemDir)
+func setParameters() map[string]string {
+	paramsDefMap := map[string]string{
+		"c": "Path to the file you want to count the bytes",
+		"l": "Path to the file you want to count the lines",
+		"w": "Path to the file you want to count the words",
+		"m": "Path to the file you want to count the characters",
+	}
 
-	return count(wcBytesReader, fileName)
+	for p, v := range paramsDefMap {
+		flag.String(p, "", v)
+	}
+
+	flag.Parse()
+
+	return paramsDefMap
 }
 
-func readNumberOfWordsFrom(wordsCountFilepath string) (int64, string) {
-	filesystemDir, fileName := parameters.GetFSandFilenameFromParameter(wordsCountFilepath)
-	wcWordsReader := readerWords.NewWcWordsReader(filesystemDir)
+func getFsReadersByParameters(params map[string]string) []FSReader {
+	var output []FSReader
 
-	return count(wcWordsReader, fileName)
+	for paramName, filepath := range params {
+		filesystemDir, fileName := splitFilepath(filepath)
+
+		maprandom := map[string]FSReader{
+			"c": {reader: readerBytes.NewWcBytesReader(filesystemDir), filename: fileName},
+			"l": {reader: readerLines.NewWcLinesReader(filesystemDir), filename: fileName},
+			"w": {reader: readerWords.NewWcWordsReader(filesystemDir), filename: fileName},
+			"m": {reader: readerChars.NewWcCharsReader(filesystemDir), filename: fileName},
+		}
+
+		output = append(output, maprandom[paramName])
+	}
+
+	return output
 }
 
-func readNumberOfCharsFrom(charsCountFilepath string) (int64, string) {
-	filesystemDir, fileName := parameters.GetFSandFilenameFromParameter(charsCountFilepath)
-	wcCharsReader := readerChars.NewWcCharsReader(filesystemDir)
-
-	return count(wcCharsReader, fileName)
+func splitFilepath(filepath string) (fs.FS, string) {
+	dirPath, fileName := utils.SplitPath(filepath)
+	filesystemDir := os.DirFS(dirPath)
+	return filesystemDir, fileName
 }
 
-func count(wcLinesReader reader.WcReaderManager, fileName string) (int64, string) {
-	counted, err := wcLinesReader.Count(fileName)
+func count(fsReader reader.WcReaderManager, fileName string) (int64, string) {
+	counted, err := fsReader.Count(fileName)
 	if err != nil {
 		fmt.Printf("can't read %s correctly\n", fileName)
 		os.Exit(1)
